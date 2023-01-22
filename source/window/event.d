e0b35011e8a92572e4bc2dd2034d436869cc5e4d
therealbluepandabear <justaguyoney@gmail.com>
2023-01-23 12:54:42 +1300

    WIP (40).

--- source/window/event.d
+++ source/window/event.d
@@ -3,5 +3,54 @@ module window.event;
 import bindbc.sfml;
 
 struct Event {
+    struct SizeEvent {
+        uint width;
+        uint height;
+    }
 
+
+    struct KeyEvent {
+
+    }
+
+    struct JoystickButtonEvent {
+
+    }
+
+    struct TouchEvent {
+        uint finger;
+        int x;
+        int y;
+    }
+
+    struct SensorEvent {
+
+    }
+
+    enum EventType {
+        Closed,
+        Resized,
+        LostFocus,
+        GainedFocus,
+        TextEntered,
+        KeyPressed,
+        KeyReleased,
+        MouseWheelScrolled,
+        MouseButtonPressed,
+        MouseButtonReleased,
+        MouseMoved,
+        MoustEntered,
+        MouseLeft,
+        JoystickButtonPressed,
+        JoystickButtonReleased,
+        JoystickMoved,
+        JoystickConnected,
+        JoystickDisconnected,
+        TouchBegan,
+        TouchMove,
+        TouchEnded,
+        SensorChanged
+    }
+
+    EventType type;
 }
