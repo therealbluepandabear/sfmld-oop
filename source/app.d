51243eda7a77c9d4a433a8835f3ede9f1b4682c2
therealbluepandabear <justaguyoney@gmail.com>
2023-01-23 00:55:37 +1300

    WIP (1).

--- source/app.d
+++ source/app.d
@@ -1,6 +1,34 @@
 import std.stdio;
+import bindbc.sfml;
+
+interface RenderTarget {
+	void clear(sfColor color);
+
+	void setView(sfView* view);
+
+	sfView* getView();
+
+	sfView* getDefaultView();
+
+	sfIntRect getViewport(sfView* view);
+
+	sfVector2f mapPixelToCoords(sfVector2i point);
+
+	sfVector2f mapPixelToCoords(sfVector2i point, sfView* view);
+
+	void pushGLStates();
+
+	void popGLStates();
+
+	void resetGLStates();
+}
+
+interface Drawable {
+	void draw();
+}
 
 void main()
 {
+	sfRectangleShape_create();
 	writeln("Edit source/app.d to start your project.");
 }
