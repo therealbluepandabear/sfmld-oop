b75cefa3c4c371c277e25b265b3aff0578d3e2a9
therealbluepandabear <justaguyoney@gmail.com>
2023-01-25 17:48:48 +1300

    More prog (19).

---
+++ source/graphics/convexshape.d
@@ -0,0 +1,31 @@
+module graphics.convexshape;
+
+import graphics.shape;
+import system.vector2f;
+
+class ConvexShape : Shape {
+    this(size_t pointCount = 0) {
+        setPointCount(pointCount);
+    }
+
+    void setPointCount(size_t pointCount) {
+        _points.length = pointCount;
+        update();
+    }
+
+    void setPoint(size_t index, Vector2f point) {
+        _points[index] = point;
+    }
+
+    override {
+        size_t getPointCount() {
+            return _points.length;
+        }
+
+        Vector2f getPoint(size_t index) {
+            return _points[index];
+        }
+    }
+
+    private Vector2f[] _points;
+}
