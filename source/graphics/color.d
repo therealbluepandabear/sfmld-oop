2dd2f58af7ba3a4f3d5de72c2dce03dc390ae8d2
therealbluepandabear <justaguyoney@gmail.com>
2023-01-27 00:29:37 +1300

    More prog (56).

--- source/graphics/color.d
+++ source/graphics/color.d
@@ -8,6 +8,20 @@ struct Color {
     ubyte blue;
     ubyte alpha = 255;
 
+    this(ubyte red, ubyte green, ubyte blue, ubyte alpha = 255) {
+        this.red = red;
+        this.green = green;
+        this.blue = blue;
+        this.alpha = alpha;
+    }
+
+    this(uint color) {
+        red = cast(ubyte)(color >> 24);
+        green = cast(ubyte)(color >> 16);
+        blue = cast(ubyte)(color >> 8);
+        alpha = cast(ubyte)(color);
+    }
+
     static const(Color) Black = Color(0, 0, 0, 255);
     static const(Color) White = Color(255, 255, 255, 255);
     static const(Color) Red = Color(255, 0, 0, 255);
@@ -16,5 +30,17 @@ struct Color {
     static const(Color) Yellow = Color(255, 255, 0, 255);
     static const(Color) Magenta = Color(255, 0, 255, 255);
     static const(Color) Cyan = Color(0, 255, 255, 255);
-    static const(Color) transparent = Color(0, 0, 0, 0);
+    static const(Color) Transparent = Color(0, 0, 0, 0);
 }
+
+import internal.testutils;
+
+mixin(unitTestStart);
+
+unittest {
+    Color black = Color(0x4D4359ff);
+
+    assert(black.red == 77 && black.green == 67 && black.blue == 89 && black.alpha == 255);
+}
+
+mixin(unitTestPassed);
