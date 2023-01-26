a4c9e62292a896beaebc6af9528aaef6f7d32446
therealbluepandabear <justaguyoney@gmail.com>
2023-01-26 16:07:35 +1300

    More prog (46).

--- source/graphics/rect.d
+++ source/graphics/rect.d
@@ -4,10 +4,10 @@ import std.traits;
 import system.vector2;
 
 struct Rect(T) if (isNumeric!(T)) {
-    T left;
-    T top;
-    T width;
-    T height;
+    T left = 0;
+    T top = 0;
+    T width = 0;
+    T height = 0;
 
     this(T left, T top, T width, T height) {
         this.left = left;
